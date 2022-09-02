fdef737c0404aacd5490a1522175024167f0c6c4
PrattayDhar <prattaygub@gmail.com>
2022-09-03 00:50:04 +0100

    load done

--- script.js
+++ script.js
@@ -18,33 +18,35 @@ loadcetagory()
 
 const call = (id) => {
     const url = `https://openapi.programming-hero.com/api/news/category/${id}`
-        fetch(url)
-            .then(res => res.json())
-            .then(data => CardDisplay(data.data))
+    fetch(url)
+        .then(res => res.json())
+        .then(data => CardDisplay(data.data))
 }
 const CardDisplay = ctagories => {
-
+    const newsportal = document.getElementById('news');
+    newsportal.innerHTML=""
     ctagories.forEach(ctagory => {
-        console.log(ctagory.title);
-        const newsportal = document.getElementById('news');
         const portalbody = document.createElement('div');
         newsportal.appendChild(portalbody);
         portalbody.innerHTML = `
         <div>
-        <div class="col-lg-12 col-md-12 col-sm-12 mb-3">
+        <div class="col-lg-12 col-md-12 col-sm-12 mb-3 ">
                     <div class="card shadow-lg p-3 mb-3 bg-white rounded  ">
-                        <img src="..." class="card-img-top" alt="...">
+                    <div class="d-lg-flex justify-content-around ">
+                    <div>
+                    <img class="thum" src="${ctagory.thumbnail_url}" class="card-img-top" alt="...">
+                    </div>
+                        
                         <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
-                                of the
-                                card's
-                                content.</p>
+                            <h5 class="card-title">${ctagory.title}</h5>
+                            <p class="card-text">${ctagory.details.slice(0, 100)}</p>
                         </div>
+                    </div>
+                    
                         <div class="card-body">
                             <div class="d-flex justify-content-around">
-                                <div><img src="#" alt="..."><span>Name</span></div>
-                                <i class="fa-regular fa-eye"></i>
+                                <div><img class="author" src="${ctagory.author.img}" alt=""><span>${ctagory.author.name}</span></div>
+                                <i class="fa-regular fa-eye">${ctagory.total_view}</i>
                                 <i class="fa-solid fa-arrow-right"></i>
                             </div>
                         </div>
