87d509f38acf70fb574b9e4cd5b49fb004a1618d
PrattayDhar <prattaygub@gmail.com>
2022-09-03 03:59:26 +0100

    try

--- script.js
+++ script.js
@@ -10,9 +10,9 @@ const displaycat = dicatagory => {
         dcatagory.appendChild(div);
         div.innerHTML = `
         <button type="button" class="btn " onclick="call('${catagory.category_id}' )">${catagory.category_name}</button>`
-     
+
     });
-  
+
 }
 loadcetagory()
 
@@ -21,7 +21,7 @@ const call = (id) => {
     fetch(url)
         .then(res => res.json())
         .then(data => CardDisplay(data.data))
-        toggolespinner(true)
+    toggolespinner(true)
 
 }
 
@@ -30,7 +30,7 @@ const CardDisplay = ctagories => {
     newscount(ctagories.length)
     const newsportal = document.getElementById('news');
 
-    newsportal.innerHTML=""
+    newsportal.innerHTML = ""
     ctagories.forEach(ctagory => {
         const portalbody = document.createElement('div');
         newsportal.appendChild(portalbody);
@@ -56,24 +56,8 @@ const CardDisplay = ctagories => {
                                 
                             <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
       <i class="fa-solid fa-arrow-right"></i>
-
 </button>
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-    ${ctagory.details}
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-      </div>
-    </div>
-  </div>
-</div>
+
 
                             </div>
                         </div>
@@ -84,17 +68,18 @@ const CardDisplay = ctagories => {
         `;
     });
     toggolespinner(false);
+    
 }
 
-const toggolespinner=isloading=>{
-    const loadsection=document.getElementById('spinner')
-    if(isloading){
+const toggolespinner = isloading => {
+    const loadsection = document.getElementById('spinner')
+    if (isloading) {
         loadsection.classList.remove('d-none');
     }
-    else{
+    else {
         loadsection.classList.add('d-none')
     }
-    
+
 }
 const newscount = (count) => {
     const DataCount = document.getElementById('counting');
